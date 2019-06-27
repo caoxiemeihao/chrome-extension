201dbd030d9ac68708e927db2b5a777254196bc4
草鞋没号 <308487730@qq.com>
2019-06-27 21:41:58 +0800

    谷歌官方教程.

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,22 @@
 
 ; (function ($) {
+  // console.log(chrome);
+
   $('#change-color').click(ev => {
     let oBtn = $(ev.target);
-    console.dir(oBtn, chrome, chrome.storage.sync);
 
-    chrome.storage.sync.get('color', data => {
-      oBtn.css('background-color', data.color).text(data.color);
-      console.log(color);
+    // chrome.storage.sync.get('color', data => {
+    //   oBtn.css('background-color', data.color).text(data.color);
+    //   console.log(data);
+    // });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        { code: `
+          document.body.style.backgroundColor = 'aliceblue';
+          ` }
+      );
     });
   });
 
